43d7f3c6e7134be39b7f777a0fc90f436d7decbb
Moveuk <v_donguk@naver.com>
2022-07-28 20:46:28 +0900

    스프링 빈 조회 - 기본

---
+++ core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,39 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext applicationContext = new
+            AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = applicationContext.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+//        applicationContext.getBean("memberService", MemberServiceImpl.class);
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean("xxxx", MemberServiceImpl.class));
+    }
+}
