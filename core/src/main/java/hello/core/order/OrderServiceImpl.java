271352c80156ffe3cebff6779f29bd6ee3eee33c
moveuk <v_donguk@naver.com>
2023-03-10 16:22:30 +0900

    의존 관계 자동 주입 - 롬복과 최신 트렌드
    RequiredArgsConstructor 사용을 통한 생성자 코드 제거
    롬복이 자바의 AnnotationProcessor를 통해 컴파일 시점에서 생성자 코드를 자동으로 생성해준다. 그러므로 class 파일에는 생성자 코드가 추가되어있다.

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,20 +3,22 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+/* @RequiredArgsConstructor 로 대체되면서 코드가 단순해짐.
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }
+    }*/
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
