39d15bc5f8464e5aa41095df1780447229614ef9
moveuk <v_donguk@naver.com>
2023-03-13 12:19:47 +0900

    실전! 스프링 부트와 JPA 활용1 - 프로젝트 환경설정
    1. 프로젝트 생성
    2. Thymeleaf 작동 확인
    3. H2 DB 작동 확인
    4. JPA DB 설정 및 p6spy 설정

---
+++ JPAshop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId(); // 커맨드와 쿼리를 분리해라. 부작용 방지를 위해 리턴값을 되도록 줄인다.
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ JPAshop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)    // Test 이 후 롤백을 진행함. 실제 db에 적용시키고 싶다면 Rollback(false) 어노테이션을 사용한다.
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member); // 같은 트랜잭션 안에서 조회하고 저장하는 것은 같은 영속성 컨텍스트 내부의 일이므로 id 값이 같기 때문에 같은 객체이다.
+
+    }
+}
