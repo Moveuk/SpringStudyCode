bd48ae3465e7cfcf2d8f3932bf497a8dab3eba82
moveuk <v_donguk@naver.com>
2023-03-22 20:49:14 +0900

    실전! 스프링 부트와 JPA 활용1 - 주문 도메인 개발1
    1. 주문, 주문상품 비즈니스 로직 개발
    2. 주문 레포지토리 및 서비스 개발

--- JPAshop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ JPAshop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,13 +1,16 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //클래스 생성 사용을 막기 위한 Protected 기본 생성자 설정(lombok)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -26,5 +29,27 @@ public class OrderItem {
 
     private int count; //주문 수량
 
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
 
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    /*
+    * 주문상품 전체 가격 조회
+    */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
