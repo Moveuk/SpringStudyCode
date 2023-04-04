d2daa9d44b5a6d3ab87b03a268e10a932b026cbd
moveuk <v_donguk@naver.com>
2023-04-04 15:15:45 +0900

    자바 ORM 표준 JPA : 고급 매핑 예제

--- ORM_JPA_Basic/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ ORM_JPA_Basic/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -20,13 +21,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Order order = new Order();
-            em.persist(order);
+            Book book = new Book();
+            book.setName("JPA");
 
-
-            OrderItem orderItem = new OrderItem();
-            order.addOrderItem(orderItem);
-            em.persist(orderItem);
+            em.persist(book);
 
             // 트랜잭션 커밋.
             tx.commit();
