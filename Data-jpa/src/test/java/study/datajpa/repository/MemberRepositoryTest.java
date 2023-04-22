481b65675831b5669ccdcf33244b9ea48877ce68
moveuk <v_donguk@naver.com>
2023-04-22 18:29:06 +0900

    스프링 데이터 JPA - 공통 인터페이스 기능: 적용
    1. JPA 기반 테스트 코드가 인터페이스 구현체에서도 작동함을 확인

--- Data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ Data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
@@ -27,5 +29,33 @@ class MemberRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(savedMember.getId());
         assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
         assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    public void basicCRUD() {
+        long standardCount = memberRepository.count();
+
+        Member member1 = Member.builder().username("member1").build();
+        Member member2 = Member.builder().username("member2").build();
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(findMember1);
+        assertThat(findMember2).isEqualTo(findMember2);
+
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2 + standardCount);
+
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2 + standardCount);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deletedCount = memberRepository.count();
+        assertThat(deletedCount).isEqualTo(standardCount);
     }
 }
